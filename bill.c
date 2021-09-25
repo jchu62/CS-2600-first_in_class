a831a8e2e9cb7599ce9dabdd45819c5e27bc1d72
Jesse Chu <jwchu@cpp.edu>
2021-09-24 19:19:03 -0700

    Added computations

--- bill.c
+++ bill.c
@@ -1,3 +1,16 @@
 // First, I will work on the tax and tip computations.
 // Then, I will work on the randomization of the 4 dishes.
 // Lastly, I will work on the input from the command line.
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(){
+float meal = 5.0;
+float tax = 5.0;
+float tip = 5.0;
+float total = meal + (meal * tax) + (meal * tip);
+printf("The meal cost is: %f\n", meal);
+printf("The tax amount is: %f\n", tax);
+printf("The tip amount is: %f\n", tip);
+printf("The total for the meal including tip and taxes is: %f\n", total);
+}
